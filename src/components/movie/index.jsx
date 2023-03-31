e2ab348b7bc918df5700a13db67c9c1906b2fab6
imandev <iman.dev989@gmail.com>
2023-03-31 19:39:05 +0330

    session 3 use axios

--- src/components/movie/index.jsx
+++ src/components/movie/index.jsx
@@ -5,6 +5,7 @@ import m4 from "../../assets/images/img4.jpg";
 import Header from "../common/Header";
 import Item from "./Item";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const data = [
   { img: m1, title: "m1", year: "1980" },
@@ -36,30 +37,54 @@ const Movie = () => {
   //   console.log("use Effect blogPosts is called", blogPosts);
   // }, [blogPosts]);
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [Loading, setLoading] = useState(false);
 
+  // useEffect(() => {
+  //   fetch("https://parseapi.back4app.com/classes/film",{
+
+  //       headers:{
+  //               "X-Parse-Application-Id": "ynw5l9xJjhSfrmK6n3U6v0krZBJf6mKiVywNNKtG",
+  //       "X-Parse-REST-API-Key":"3jTFIn0TkJA22iw9uPUWvz2vIARAFKeM2VN8I2B9",
+  //           }
+  //       }).then(
+  //     (res) => {
+  //       console.log(res);
+  //       res.json().then((info) => {
+  //         console.log(info);
+  //         setData(info.results);
+  //         setLoading(false);
+  //       });
+  //     },
+  //     (error) => {
+  //       setLoading(false);
+  //       alert("error");
+  //       console.log(error);
+  //     }
+  //   );
+  // }, []);
   useEffect(() => {
-    fetch("https://parseapi.back4app.com/classes/film",{
-        
-        headers:{
-                "X-Parse-Application-Id": "ynw5l9xJjhSfrmK6n3U6v0krZBJf6mKiVywNNKtG",
-        "X-Parse-REST-API-Key":"3jTFIn0TkJA22iw9uPUWvz2vIARAFKeM2VN8I2B9",
-            }
-        }).then(
-      (res) => {
-        console.log(res);
-        res.json().then((info) => {
-          console.log(info);
-          setData(info.results);
-          setLoading(false);
-        });
+    setLoading(true);
+    let config = {
+      headers: {
+        "X-Parse-Application-Id": "ynw5l9xJjhSfrmK6n3U6v0krZBJf6mKiVywNNKtG",
+        "X-Parse-REST-API-Key": "3jTFIn0TkJA22iw9uPUWvz2vIARAFKeM2VN8I2B9",
       },
-      (error) => {
+    };
+    axios
+      .get("https://parseapi.back4app.com/classes/film", config)
+      .then(function (response) {
+        //handle success
+        console.log(response);
+        console.log(response.data.results);
+        setData(response.data.results);
         setLoading(false);
-        alert("error");
+      })
+      .catch(function (error) {
+        //handle error
+        setLoading(false);
+
         console.log(error);
-      }
-    );
+      });
   }, []);
   return (
     <div className="container">
@@ -67,9 +92,13 @@ const Movie = () => {
         <Header />
       </div>
       <div className="row">
-        {data.map((i, index) => (
-          <Item key={index} img={i.img} title={i.name} year={i.year} />
-        ))}
+        {Loading ? (
+          <p className="text-center bg-danger alert">Loading</p>
+        ) : (
+          data.map((i, index) => (
+            <Item key={index} img={i.img} title={i.name} year={i.year} />
+          ))
+        )}
         {/* <button
           className="btn btn-success btn-small"
           onClick={() => onYesPress()}
